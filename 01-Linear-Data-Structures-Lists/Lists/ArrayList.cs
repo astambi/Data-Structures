9e29222c955154e2a1fe06fcd217291fa2a35e05
Anna <annsta@gmail.com>
2018-01-09 21:35:01 +0200

    Linear Data Structures - Lists

---
+++ 01-Linear-Data-Structures-Lists/Lists/ArrayList.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+public class ArrayList<T>
+{
+    private const int InitialCapacity = 2;
+    private const int ResizeMultiple = 2;
+    private const int DownsizeTrigger = 4;
+
+    private T[] collection;
+
+    public ArrayList(int capacity = InitialCapacity)
+    {
+        this.collection = new T[capacity];
+        this.Capacity = capacity;
+    }
+
+    private int Capacity { get; set; }
+
+    public int Count { get; private set; }
+
+    public T this[int index]
+    {
+        get
+        {
+            this.ValidateIndex(index);
+            return this.collection[index];
+        }
+
+        set
+        {
+            this.ValidateIndex(index);
+            this.collection[index] = value;
+        }
+    }
+
+    public void Add(T item)
+    {
+        if (this.Count == this.Capacity)
+        {
+            this.Resize();
+        }
+
+        this.collection[this.Count++] = item;
+    }
+
+    public T RemoveAt(int index)
+    {
+        var removedElement = this[index];
+        this[index] = default(T);
+
+        this.ShiftLeft(index);
+        this.Count--;
+
+        if (this.Count <= this.Capacity / DownsizeTrigger)
+        {
+            this.Downsize();
+        }
+
+        return removedElement;
+    }
+
+    private void Downsize()
+    {
+        this.Capacity /= ResizeMultiple;
+
+        var downsizedCollection = new T[this.Capacity];
+        Array.Copy(this.collection, downsizedCollection, this.Count);
+        this.collection = downsizedCollection;
+    }
+
+    private void Resize()
+    {
+        this.Capacity *= ResizeMultiple;
+
+        var resizedCollection = new T[this.Capacity];
+        Array.Copy(this.collection, resizedCollection, this.Count);
+        this.collection = resizedCollection;
+    }
+
+    private void ShiftLeft(int index)
+    {
+        for (int i = index; i < this.Count; i++)
+        {
+            this.collection[i] = this.collection[i + 1];
+        }
+    }
+
+    private void ValidateIndex(int index)
+    {
+        if (index < 0 || index >= this.Count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+    }
+}
---
+++ 01-Linear-Data-Structures-Lists/Lists/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var collection = new ArrayList<int>();
+
+        collection.Add(10);
+        Console.WriteLine(collection.Count);
+
+        var element = collection.RemoveAt(0);
+        Console.WriteLine(element);
+        Console.WriteLine(collection.Count);
+    }
+}
