2275cee88bb280b8d77692905db34bfb8b25cc4c
Anna <annsta@gmail.com>
2018-02-07 00:39:05 +0200

    B-Trees & Red-Black Trees

---
+++ 06-B-Trees & Red-Black-Trees/RedBlackTree/IBinarySearchTree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface IBinarySearchTree<T> where T: IComparable
+{
+    //Basic Tree Operations
+    bool Contains(T element);
+
+    void EachInOrder(Action<T> action);
+
+    void Insert(T element);
+
+    //Binary Search Tree Operations
+    int Count();
+
+    void DeleteMin();
+
+    void DeleteMax();
+
+    void Delete(T element);
+
+    IEnumerable<T> Range(T startRange, T endRange);
+
+    int Rank(T element);
+
+    IBinarySearchTree<T> Search(T element);
+
+    T Select(int rank);
+
+    T Ceiling(T element);
+
+    T Floor(T element);
+}
