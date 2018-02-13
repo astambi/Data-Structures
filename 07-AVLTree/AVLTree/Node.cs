6a0568fe89982e8f5cc444fb2fe8f3998210fa95
Anna <annsta@gmail.com>
2018-02-13 23:37:30 +0200

    AVL Tree

---
+++ 07-AVLTree/AVLTree/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Node<T> where T : IComparable<T>
+{
+    public Node(T value)
+    {
+        this.Value = value;
+        this.Height = 1;
+    }
+
+    public T Value;
+
+    public Node<T> Left;
+
+    public Node<T> Right;
+
+    public int Height;
+}
