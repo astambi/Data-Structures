13dfb931fbf0b9c6e8b53005269c563416992492
Anna <annsta@gmail.com>
2018-02-20 20:42:12 +0200

    AVL & AA-Trees

---
+++ 07-First-Last-List/First-Last-List/FirstLastList.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+public class FirstLastList<T> : IFirstLastList<T> where T : IComparable<T>
+{
+    private LinkedList<T> listByInsertion;
+    private OrderedBag<LinkedListNode<T>> listAsc;
+    private OrderedBag<LinkedListNode<T>> listDesc;
+
+    public FirstLastList()
+    {
+        this.listByInsertion = new LinkedList<T>();
+        this.listAsc = new OrderedBag<LinkedListNode<T>>((x, y) => x.Value.CompareTo(y.Value));
+        this.listDesc = new OrderedBag<LinkedListNode<T>>((x, y) => y.Value.CompareTo(x.Value));
+    }
+
+    public int Count
+        => this.listByInsertion.Count;
+
+    public void Add(T element)
+    {
+        var node = new LinkedListNode<T>(element);
+
+        this.listByInsertion.AddLast(node);
+        this.listAsc.Add(node);
+        this.listDesc.Add(node);
+    }
+
+    public void Clear()
+    {
+        this.listByInsertion.Clear();
+        this.listAsc.Clear();
+        this.listDesc.Clear();
+    }
+
+    public IEnumerable<T> First(int count)
+    {
+        if (!IsValidCount(count))
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        var current = this.listByInsertion.First;
+        int index = 0;
+
+        while (index++ < count)
+        {
+            yield return current.Value;
+
+            current = current.Next;
+        }
+    }
+
+    public IEnumerable<T> Last(int count)
+    {
+        if (!IsValidCount(count))
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        var current = this.listByInsertion.Last;
+        int index = 0;
+
+        while (index++ < count)
+        {
+            yield return current.Value;
+
+            current = current.Previous;
+        }
+    }
+
+    public IEnumerable<T> Max(int count)
+    {
+        if (!IsValidCount(count))
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        return this.listDesc
+           .Take(count)
+           .Select(e => e.Value)
+           .ToList();
+    }
+
+    public IEnumerable<T> Min(int count)
+    {
+        if (!IsValidCount(count))
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        return this.listAsc
+            .Take(count)
+            .Select(e => e.Value)
+            .ToList();
+    }
+
+    public int RemoveAll(T element)
+    {
+        var elementNode = new LinkedListNode<T>(element);
+
+        this.listAsc
+            .Range(elementNode, true, elementNode, true)
+            .ForEach(e => this.listByInsertion.Remove(e));
+
+        this.listDesc.RemoveAllCopies(elementNode);
+
+        return this.listAsc.RemoveAllCopies(elementNode);
+    }
+
+    private bool IsValidCount(int count)
+        => 0 <= count && count <= this.Count;
+}
---
+++ 07-First-Last-List/First-Last-List/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wintellect.PowerCollections;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        FirstLastList<int> list = new FirstLastList<int>();
+        list.Add(1);
+        list.Add(1);
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.Add(4);
+        Console.WriteLine();
+
+        //Console.WriteLine(list.Count);
+        //Console.WriteLine(string.Join(" ", list.First(2)));
+        //Console.WriteLine(string.Join(" ", list.Last(1)));
+
+        //Console.WriteLine(string.Join(" ", list.Max(3)));
+        //Console.WriteLine(string.Join(" ", list.Max(0)));
+        //Console.WriteLine(string.Join(" ", list.Min(5)));
+
+        //Console.WriteLine(string.Join(" ", list.Min(3)));
+        //Console.WriteLine(string.Join(" ", list.Min(0)));
+        //Console.WriteLine(string.Join(" ", list.Max(5)));
+
+        //list.Clear();
+        //Console.WriteLine(list.Count);
+
+        //list.RemoveAll(1);
+        //Console.WriteLine(list.Count);
+        //Console.WriteLine(string.Join(" ", list.Max(3)));
+    }
+}
