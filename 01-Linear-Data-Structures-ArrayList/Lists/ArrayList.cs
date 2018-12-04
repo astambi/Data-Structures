63fc8ea3c0c356951ebfa328a22c79a91469ed52
Anna Stambolieva <astambi@users.noreply.github.com>
2018-12-04 22:26:02 +0200

    Linear Data Structures - ArrayList

---
+++ 01-Linear-Data-Structures-ArrayList/Lists/ArrayList.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+public class ArrayList<T>
+{
+    private const int InitialCapacity = 2;
+    private const int IncreaseMultiple = 2;
+    private const int DecreaseMultiple = 4;
+    private T[] collection;
+
+    public ArrayList(int capacity = InitialCapacity)
+    {
+        this.collection = new T[capacity];
+    }
+
+    public int Count { get; private set; }
+
+    public T this[int index]
+    {
+        get
+        {
+            this.Validate(index);
+            return this.collection[index];
+        }
+
+        set
+        {
+            this.Validate(index);
+            this.collection[index] = value;
+        }
+    }
+
+    public void Add(T item)
+    {
+        if (this.Count >= this.collection.Length)
+        {
+            this.ResizeCollection(this.collection.Length * IncreaseMultiple);
+        }
+
+        this.collection[this.Count++] = item;
+    }
+
+    public T RemoveAt(int index)
+    {
+        var removedElement = this[index]; // with validation
+        this[index] = default(T);
+        this.Count--;
+        this.ShiftLeft(index);
+
+        if (this.Count <= this.collection.Length / DecreaseMultiple)
+        {
+            this.ResizeCollection(this.collection.Length / DecreaseMultiple);
+        }
+
+        return removedElement;
+    }
+
+    private void Validate(int index)
+    {
+        if (index < 0 || index >= this.Count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    private void ResizeCollection(int size)
+    {
+        var collectionCopy = new T[size];
+        Array.Copy(this.collection, collectionCopy, this.Count);
+        this.collection = collectionCopy;
+    }
+
+    private void ShiftLeft(int index)
+    {
+        for (int i = index; i < this.Count; i++)
+        {
+            this.collection[i] = this.collection[i + 1];
+        }
+    }
+}
---
+++ 01-Linear-Data-Structures-ArrayList/Lists/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var list = new ArrayList<int>();
+
+        list.Add(-100);
+        list.Add(200);
+        list.Add(300);
+        list.Add(400);
+        Print(list);
+
+        list[0] = list[0] * -1;
+        Print(list);
+
+        try
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                var element = list.RemoveAt(list.Count - 1);
+                Console.WriteLine($"Removed: {element}");
+                Print(list);
+
+                element = list.RemoveAt(0);
+                Console.WriteLine($"Removed: {element}");
+                Print(list);
+            }
+
+            var invalidElem = list.RemoveAt(100);
+            Console.WriteLine($"Removed: {invalidElem}");
+            Print(list);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Error: Index out of range");
+        }
+    }
+
+    private static void Print(ArrayList<int> list)
+    {
+        Console.WriteLine($"Count: {list.Count}");
+        for (int i = 0; i < list.Count; i++)
+        {
+            Console.WriteLine(list[i]);
+        }
+    }
+}
