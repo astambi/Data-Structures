1ab8ee52149371021e009253f146c4e8c9a6d034
Anna Stambolieva <astambi@users.noreply.github.com>
2018-12-05 03:30:43 +0200

    Linear Data Structures - ReversedList

---
+++ 01-Linear-Data-Structures-Lists/ReversedList/ReversedList.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ReversedList<T> : IEnumerable<T>
+{
+    private const int InitialCapacity = 2;
+    private const int ResizeMultiple = 2;
+    private T[] collection;
+
+    public ReversedList()
+    {
+        this.collection = new T[InitialCapacity];
+    }
+
+    public void Add(T item)
+    {
+        if (this.collection.Length == this.Count)
+        {
+            this.Resize(this.Count * ResizeMultiple);
+        }
+
+        this.collection[this.Count++] = item;
+    }
+
+    public int Capacity => this.collection.Length;
+
+    public int Count { get; private set; }
+
+    public T this[int index]
+    {
+        get
+        {
+            this.Validate(index);
+            return this.collection[this.ReversedIndex(index)];
+        }
+
+        set
+        {
+            this.Validate(index);
+            this.collection[this.ReversedIndex(index)] = value;
+        }
+    }
+
+    public T RemoveAt(int index)
+    {
+        this.Validate(index);
+
+        var removedItem = this[index];
+        this[index] = default(T);
+        this.ShiftLeft(this.ReversedIndex(index));
+        this.Count--;
+
+        return removedItem;
+    }
+
+    private void ShiftLeft(int index)
+    {
+        for (int i = index; i < this.Count - 1; i++)
+        {
+            this.collection[i] = this.collection[i + 1];
+        }
+
+        this.collection[this.Count - 1] = default(T);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < this.Count; i++)
+        {
+            yield return this[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+    private int ReversedIndex(int index) => this.Count - 1 - index;
+
+    private void Resize(int newCapacity)
+    {
+        var resizedCollection = new T[newCapacity];
+        Array.Copy(this.collection, resizedCollection, this.Count);
+        this.collection = resizedCollection;
+    }
+
+    private void Validate(int index)
+    {
+        if (index < 0 || index >= this.Count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+    }
+}
---
+++ 01-Linear-Data-Structures-Lists/ReversedList/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        var reversedList = new ReversedList<int>();
+
+        // Add
+        reversedList.Add(100);
+        reversedList.Add(200);
+        reversedList.Add(300);
+        reversedList.Add(-400);
+
+        // Indexer
+        Console.WriteLine(reversedList[0]);
+        reversedList[0] *= -1;
+        Console.WriteLine(reversedList[0]);
+
+        Print(reversedList);
+
+        // RemoveAt
+        for (int i = 0; i < 2; i++)
+        {
+            Console.WriteLine($"Removed: {reversedList.RemoveAt(reversedList.Count - 1)}");
+            Print(reversedList);
+
+            Console.WriteLine($"Removed: {reversedList.RemoveAt(0)}");
+            Print(reversedList);
+        }
+    }
+
+    private static void Print(ReversedList<int> reversedList)
+    {
+        Console.WriteLine($"Capacity: {reversedList.Capacity}");
+        Console.WriteLine($"Count: {reversedList.Count}");
+
+        foreach (var item in reversedList)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
