27ed2eff7f86e1390af07eeaa19896bae031ecea
Anna Stambolieva <astambi@users.noreply.github.com>
2019-01-24 21:41:50 +0200

    AVL Trees First-Last List

---
+++ 07-AVL Trees - First-Last List/First-Last-List/FirstLastList.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+public class FirstLastList<T> : IFirstLastList<T> where T : IComparable<T>
+{
+    private readonly LinkedList<T> linkedList = new LinkedList<T>(); // by insertion
+    private readonly OrderedBag<LinkedListNode<T>> listAsc = new OrderedBag<LinkedListNode<T>>(
+        (a, b) => a.Value.CompareTo(b.Value));
+    private readonly OrderedBag<LinkedListNode<T>> listDesc = new OrderedBag<LinkedListNode<T>>(
+        (a, b) => b.Value.CompareTo(a.Value));
+
+    public int Count
+        => this.linkedList.Count;
+
+    public void Add(T element)
+    {
+        var node = new LinkedListNode<T>(element); //
+
+        this.linkedList.AddLast(node);
+        this.listAsc.Add(node);
+        this.listDesc.Add(node);
+    }
+
+    public void Clear()
+    {
+        this.linkedList.Clear();
+        this.listAsc.Clear();
+        this.listDesc.Clear();
+    }
+
+    public IEnumerable<T> First(int count)
+    {
+        this.Validate(count);
+
+        var current = this.linkedList.First;
+        var countResult = 0;
+
+        while (countResult < count)
+        {
+            yield return current.Value;
+            current = current.Next;
+            countResult++;
+        }
+    }
+
+    public IEnumerable<T> Last(int count)
+    {
+        this.Validate(count);
+
+        var current = this.linkedList.Last;
+        var countResult = 0;
+
+        while (countResult < count)
+        {
+            yield return current.Value;
+            current = current.Previous;
+            countResult++;
+        }
+    }
+
+    public IEnumerable<T> Max(int count)
+    {
+        this.Validate(count);
+
+        return this.listDesc
+            .Take(count)
+            .Select(n => n.Value);
+    }
+
+    public IEnumerable<T> Min(int count)
+    {
+        this.Validate(count);
+
+        return this.listAsc
+            .Take(count)
+            .Select(n => n.Value);
+    }
+
+    public int RemoveAll(T element)
+    {
+        var node = new LinkedListNode<T>(element);
+
+        this.listAsc
+            .Range(node, true, node, true) // [node, node]
+            .ForEach(n => this.linkedList.Remove(n));
+
+        var countRemoved = this.listAsc.RemoveAllCopies(node);
+        this.listDesc.RemoveAllCopies(node);
+
+        return countRemoved;
+    }
+
+    private void Validate(int count)
+    {
+        if (!this.IsValid(count))
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    private bool IsValid(int count)
+        => 0 <= count && count <= this.Count;
+}
---
+++ 07-AVL Trees - First-Last List/First-Last-List/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public class Program
+{
+    private const int count = 4;
+
+    private class Product : IComparable<Product>
+    {
+        public Product(decimal price, string title = null)
+        {
+            this.Price = price;
+            this.Title = title;
+        }
+
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int CompareTo(Product other)
+            => this.Price.CompareTo(other.Price);
+
+        public override string ToString()
+            => $"{this.Price} - {this.Title}";
+    }
+
+    public static void Main()
+    {
+        var products = new FirstLastList<Product>();
+        products.Add(new Product(0.50m, "coffee"));
+        products.Add(new Product(1.20m, "mint drops"));
+        products.Add(new Product(1.20m, "beer"));
+        products.Add(new Product(0.35m, "candy"));
+        products.Add(new Product(1.20m, "cola"));
+
+        Console.WriteLine("========================= Count");
+        Console.WriteLine(products.Count);
+        Console.WriteLine(string.Join(Environment.NewLine, products.First(products.Count)));
+        Console.WriteLine("========================= First");
+        Console.WriteLine(string.Join(Environment.NewLine, products.First(count)));
+        Console.WriteLine("========================= Last");
+        Console.WriteLine(string.Join(Environment.NewLine, products.Last(count)));
+        Console.WriteLine("========================= Min");
+        Console.WriteLine(string.Join(Environment.NewLine, products.Min(count)));
+        Console.WriteLine("========================= Max");
+        Console.WriteLine(string.Join(Environment.NewLine, products.Max(count)));
+        Console.WriteLine("========================= Remove All @ Price 1.20");
+        var product = new Product(1.20m);
+        Console.WriteLine($"Removed {products.RemoveAll(product)}");
+        Console.WriteLine(string.Join(Environment.NewLine, products.First(products.Count)));
+        Console.WriteLine("========================= Clear");
+        products.Clear();
+        Console.WriteLine(products.Count);
+    }
+}
