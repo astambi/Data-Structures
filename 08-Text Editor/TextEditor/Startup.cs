65c7298f3983c0245f749c4606243284d14db131
Anna Stambolieva <astambi@users.noreply.github.com>
2019-02-03 14:05:13 +0200

    Text Editor (Trie & Dictionary)

---
+++ 08-Text Editor/TextEditor/Startup.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public class Startup
+{
+    public static void Main()
+    {
+        var textEditor = new TextEditor();
+        var regex = new Regex("\"(.*)\"");
+
+        while (true)
+        {
+            var line = Console.ReadLine();
+            if (line == "end")
+            {
+                break;
+            }
+
+            var match = regex.Match(line);
+            var str = string.Empty;
+            if (match.Success)
+            {
+                str = match.Groups[1].Value;
+                line = line.Substring(0, line.IndexOf(str) - 1);
+            }
+
+            var commandArgs = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var command = commandArgs[0];
+
+            switch (command)
+            {
+                // {command} {username} {params}
+                case "login": ProcessLogin(textEditor, commandArgs); break;
+                case "logout": ProcessLogout(textEditor, commandArgs); break;
+                case "users": ProcessUsers(textEditor, commandArgs); break;
+                default:
+                    // {username} {command} {params}
+                    var username = commandArgs[0];
+
+                    // Ignore commands if user is not logged in
+                    if (!textEditor.Users(username).Contains(username))
+                    {
+                        break;
+                    }
+
+                    command = commandArgs[1];
+                    commandArgs = commandArgs.Skip(2).ToArray();
+
+                    switch (command)
+                    {
+                        case "insert": ProcessInsert(textEditor, username, commandArgs, str); break;
+                        case "prepend": textEditor.Prepend(username, str); break;
+                        case "substring": ProcessSubstring(textEditor, username, commandArgs); break;
+                        case "delete": ProcessDelete(textEditor, username, commandArgs); break;
+                        case "clear": textEditor.Clear(username); break;
+                        case "length": Console.WriteLine(textEditor.Length(username)); break;
+                        case "print": Console.WriteLine(textEditor.Print(username)); break;
+                        case "undo": textEditor.Undo(username); break;
+                        default: break;
+                    }
+                    break;
+            }
+        }
+    }
+
+    private static void ProcessLogin(TextEditor textEditor, string[] commandArgs)
+    {
+        if (commandArgs.Length > 1)
+        {
+            var username = commandArgs[1];
+            textEditor.Login(username);
+        }
+    }
+
+    private static void ProcessLogout(TextEditor textEditor, string[] commandArgs)
+    {
+        if (commandArgs.Length > 1)
+        {
+            var username = commandArgs[1];
+            textEditor.Logout(username);
+        }
+    }
+
+    private static void ProcessUsers(TextEditor textEditor, string[] commandArgs)
+    {
+        var prefix = commandArgs.Length > 1 ? commandArgs[1] : string.Empty;
+        var users = textEditor.Users(prefix);
+        foreach (var user in users)
+        {
+            Console.WriteLine(user);
+        }
+    }
+
+    private static void ProcessInsert(TextEditor textEditor, string username, string[] commandArgs, string str)
+    {
+        if (commandArgs.Length > 0)
+        {
+            var index = int.Parse(commandArgs[0]);
+            textEditor.Insert(username, index, str);
+        }
+    }
+
+    private static void ProcessSubstring(TextEditor textEditor, string username, string[] commandArgs)
+    {
+        if (commandArgs.Length > 1)
+        {
+            var index = int.Parse(commandArgs[0]);
+            var length = int.Parse(commandArgs[1]);
+            textEditor.Substring(username, index, length);
+        }
+    }
+
+    private static void ProcessDelete(TextEditor textEditor, string username, string[] commandArgs)
+    {
+        if (commandArgs.Length > 1)
+        {
+            var index = int.Parse(commandArgs[0]);
+            var length = int.Parse(commandArgs[1]);
+            textEditor.Delete(username, index, length);
+        }
+    }
+}
---
+++ 08-Text Editor/TextEditor/TextEditor.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class TextEditor : ITextEditor
+{
+    //// NB Dictionary uses less memory than Trie => comment/uncomment to compare performance
+
+    //private readonly Trie<StringBuilder> usersCurrent = new Trie<StringBuilder>(); // Trie
+    private readonly Dictionary<string, StringBuilder> usersCurrent = new Dictionary<string, StringBuilder>(); // Dict
+    private readonly Dictionary<string, Stack<StringBuilder>> usersCache = new Dictionary<string, Stack<StringBuilder>>();
+
+    public void Clear(string username)
+    {
+        if (!this.IsLoggedIn(username)) return;
+
+        this.Cache(username);
+        this.GetCurrent(username).Clear();
+    }
+
+    public void Delete(string username, int startIndex, int length)
+    {
+        if (!this.IsLoggedIn(username)) return;
+
+        this.Cache(username);
+        this.GetCurrent(username).Remove(startIndex, length);
+    }
+
+    public void Insert(string username, int index, string str)
+    {
+        if (!this.IsLoggedIn(username)) return;
+
+        this.Cache(username);
+        this.GetCurrent(username).Insert(index, str);
+    }
+
+    public int Length(string username)
+        => this.IsLoggedIn(username)
+        ? this.GetCurrent(username).Length
+        : 0;
+
+    public void Login(string username)
+    {
+        if (!this.IsValid(username)) return;
+
+        this.UpdateCurrent(username, new StringBuilder());
+        this.usersCache[username] = new Stack<StringBuilder>();
+    }
+
+    public void Logout(string username)
+    {
+        if (!this.IsLoggedIn(username)) return;
+
+        this.RemoveCurrent(username);
+        this.usersCache.Remove(username);
+    }
+
+    public void Prepend(string username, string str)
+    {
+        if (!this.IsLoggedIn(username)) return;
+
+        this.Cache(username);
+        this.GetCurrent(username).Insert(0, str);
+    }
+
+    public string Print(string username)
+        => this.IsLoggedIn(username)
+        ? this.GetCurrent(username).ToString()
+        : string.Empty;
+
+    public void Substring(string username, int startIndex, int length)
+    {
+        if (!this.IsLoggedIn(username)) return;
+
+        this.Cache(username);
+        var current = this.GetCurrent(username);
+        var substr = this.GetSubstring(current, startIndex, length);
+        this.UpdateCurrent(username, substr);
+    }
+
+    public void Undo(string username)
+    {
+        if (!this.IsLoggedIn(username)) return;
+
+        var cacheStack = this.usersCache[username];
+        if (cacheStack.Any())
+        {
+            var cache = cacheStack.Pop();
+            this.UpdateCurrent(username, cache);
+        }
+    }
+
+    public IEnumerable<string> Users(string prefix = "")
+        => prefix == string.Empty
+        ? this.usersCache.Keys
+        : this.usersCache.Keys.Where(u => u.StartsWith(prefix));
+
+    private void Cache(string username)
+    {
+        var current = this.GetCurrent(username);
+        var cache = new StringBuilder().Append(current);
+        this.usersCache[username].Push(cache);
+    }
+
+    private bool CurrentContains(string username)
+        //=> this.usersCurrent.Contains(username); // Trie
+        => this.usersCurrent.ContainsKey(username); // Dict
+
+    private StringBuilder GetCurrent(string username)
+        //=> this.usersCurrent.GetValue(username); // Trie
+        => this.usersCurrent[username]; // Dict
+
+    private StringBuilder GetSubstring(StringBuilder current, int startIndex, int length)
+    {
+        var builder = new StringBuilder();
+        for (int i = startIndex; i < length; i++)
+        {
+            builder.Append(current[i]);
+        }
+
+        return builder;
+    }
+
+    private bool IsLoggedIn(string username)
+        => this.usersCache.ContainsKey(username)
+        && this.CurrentContains(username)
+        ? true
+        : false;
+
+    private bool IsValid(string username)
+        => !string.IsNullOrWhiteSpace(username);
+
+    private void RemoveCurrent(string username)
+        //=> this.UpdateCurrent(username, null); // Trie
+        => this.usersCurrent.Remove(username); // Dict
+
+    private void UpdateCurrent(string username, StringBuilder value)
+        //=> this.usersCurrent.Insert(username, value); // Trie
+        => this.usersCurrent[username] = value; // Dict
+}
